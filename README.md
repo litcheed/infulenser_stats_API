87a038925e704822d088ae72686fd6ed1dfa74be
litcheed <litcheed@gmail.com>
2024-04-04 20:50:03 +0900

    feat: markdownやメインファイルコメント調整

--- README.md
+++ README.md
@@ -1,35 +1,81 @@
-### ツール実行
+# 準備
 
-ビルド
-```
-docker build -t api-image .
-```
-コンテナ起動
-```
-docker run -it api-image /bin/bash
+### CSVデータをDBへ格納するツール実行手順(初回のみ実行)
 
-```
+1. "\infulenser_stats_API\app\tools\csv"配下にcsvデータを配置
 
+<br>
+
+2. ターミナルからビルド実行
+```
+docker-compose up --build
+```
+<br>
+
+3. ビルド完了後、コンテナは閉じないまま別ターミナルを開きpythonコンテナのbash環境にアクセス
+```
+docker exec -it infulenser_stats_api-python-1 bash
+```
+<br>
+
+4. bashアクセス後、カレントディレクトリが"/usr/src/python"であることを確認
 ```
 pwd
-/usr/src/pythonであることを確認。
+```
+<br>
+
+5. CSVデータをDBへ格納するツール実行()
+```
 python app/tools/csv_to_db.py app/tools/csv/t_influencer_posts_202401121334.csv
 ```
+<br>
+<br>
 
-docker build -t api-image .
-docker run  --rm -it api-image /bin/bash
+# API実行
 
+1. ターミナルからビルド実行
+```
+docker-compose up --build
+```
+<br>
 
+2. ビルド完了後、起動したコンテナは閉じないまま別ターミナルを開きpythonコンテナのbash環境にアクセス
+```
+docker exec -it infulenser_stats_api-python-1 bash
+```
+<br>
 
+3. 下記コマンドで各APIを実行
+<br>
+平均いいね数、平均コメント数を取得するAPI以外は?top_n=5の数値を別の数値に変えることで上位N件の件数を調整可能
+
+- 平均いいね数、平均コメント数を取得するAPI
+```
 curl -X 'GET' 'http://127.0.0.1:8000/stats/average-likes-comments' -H 'accept: application/json' -w "\n"
+```
+<br>
+
+- 平均いいね数の多いインフルエンサー上位N件を取得するAPI
+```
 curl -X 'GET' 'http://127.0.0.1:8000/influencers/top-by-likes?top_n=5' -H 'accept: application/json' -w "\n"
+```
+<br>
 
+- 平均コメント数の多いインフルエンサー上位N件を取得するAPI
+```
 curl -X 'GET' 'http://127.0.0.1:8000/influencers/top-by-comments?top_n=5' -H 'accept: application/json' -w "\n"
+```
+<br>
+
+- textカラムに頻出の名詞上位N件を取得するAPI
+```
 curl -X 'GET' 'http://127.0.0.1:8000/content/top-nouns?top_n=5' -H 'accept: application/json' -w "\n"
+```
+<br>
+<br>
 
-
-
-
-
-テスト実行
+## テスト実行
+上記手順2まで完了したら下記コマンドを入力
+```
 pytest -s app/test_main_api.py
+```
--- app/main_api.py
+++ app/main_api.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-#######################################################
+#################################################################
 #
 # 概要:APIメインファイル
+#      1️⃣平均いいね数、平均コメント数を取得するAPI
+#      2️⃣平均いいね数の多いインフルエンサー上位N件を取得するAPI
+#      3️⃣平均コメント数の多いインフルエンサー上位N件を取得するAPI
+#      4️⃣textカラムに頻出の名詞上位N件を取得するAPI
 #
-#######################################################
+#################################################################
 
 import re
 import sys
@@ -108,7 +112,7 @@ async def get_counts_good_cmnt():
 # 2️⃣平均いいね数の多いインフルエンサー上位N件を取得
 ####################################################################
 @app.get("/influencers/top-by-likes")
-async def get_ave_good_topN(top_n: int = Query(default=10, alias='top_n')) -> List[Dict[str, float]]:
+async def get_ave_good_topN(top_n: int = Query(default=10, alias='top_n')):
 
     #---------------#
     # DB接続
@@ -177,79 +181,7 @@ async def get_ave_good_topN(top_n: int = Query(default=10, alias='top_n')) -> Li
 
 
 ####################################################################
-# 3️⃣平均フォロワー数の多いインフルエンサー上位N件を取得
-####################################################################
-@app.get("/influencers/top-by-followers")
-async def get_ave_flwr_topN(top_n: int = Query(default=10, alias='top_n')):
-
-    #---------------#
-    # DB接続
-    #---------------#
-    try:
-        conn, cursor = cmn_session_db.conn_db()
-        
-    except Exception:
-        print(f"{cmn_msg.ERR_MSG}DB接続に失敗したためアプリを終了します。")
-        sys.exit(1)
-    
-
-    #---------------#
-    # データ取得
-    #---------------#
-    try:
-        # 使用するテーブルを選択
-        cursor.execute(f"USE {table_name}")
-
-        # DBからデータ取得
-        cursor.execute("""
-            SELECT influencer_id, AVG(likes) AS avg_likes
-            FROM influencers_posts
-            GROUP BY influencer_id
-            ORDER BY avg_likes DESC
-            LIMIT %s
-        """, (top_n,))
-
-    except Exception as e:
-        print(f"{cmn_msg.WARN_MSG}クエリ実行に失敗しました。{e}")
-
-
-    #---------------#
-    # 実行結果整形
-    #---------------#
-    try:
-        # クエリ実行結果の取得
-        results = cursor.fetchall()
-        
-        # JSON形式に整形
-        result = [
-            {"influencer_id": result[0], "average_likes": float(result[1])}
-            for result in results
-        ]
-    
-    except Exception as e:
-        print(f"{cmn_msg.WARN_MSG}実行結果の整形に失敗しました。\n{e}")
-    
-
-    #---------------#
-    # DB切断
-    #---------------#
-    try:
-        cmn_session_db.disconn_db(conn, cursor)
-        
-    except Exception:
-        print(f"{cmn_msg.ERR_MSG}DB切断に失敗したためアプリを終了します。")
-        sys.exit(1)
-    
-
-    #---------------#
-    # 結果出力
-    #---------------#
-    return result
-
-
-
-####################################################################
-# 4️⃣平均コメント数の多いインフルエンサー上位N件を取得
+# 3️⃣平均コメント数の多いインフルエンサー上位N件を取得
 ####################################################################
 @app.get("/influencers/top-by-comments")
 async def get_ave_cmnt_topN(top_n: int = Query(default=10, alias='top_n')):
@@ -321,7 +253,7 @@ async def get_ave_cmnt_topN(top_n: int = Query(default=10, alias='top_n')):
 
 
 ####################################################################
-# 5️⃣textカラムに頻出の名詞上位N件を取得
+# 4️⃣textカラムに頻出の名詞上位N件を取得
 ####################################################################
 @app.get("/content/top-nouns")
 async def get_noun_topN(top_n: int = Query(default=10, alias='top_n')):

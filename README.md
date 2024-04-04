514b7e2db7abf7835f17cf9de1c057859b60094b
litcheed <litcheed@gmail.com>
2024-04-04 11:35:11 +0900

    feat: API実装 curl接続確認後

--- README.md
+++ README.md
@@ -19,3 +19,15 @@ python app/tools/csv_to_db.py app/tools/csv/t_influencer_posts_202401121334.csv
 docker build -t api-image .
 docker run  --rm -it api-image /bin/bash
 
+
+
+nohup uvicorn app.main_api:app --reload > uvicorn.log 2>&1 &
+
+実行中プロセス確認
+jobs
+
+ジョブ番号確認
+ps
+
+ジョブの停止
+kill ジョブ番号
--- app/main_api.py
+++ app/main_api.py
@@ -5,8 +5,8 @@
 #
 #######################################################
 
-import time
 import sys
+sys.path.append("/usr/src/python/app")
 import json
 from fastapi import FastAPI, Query
 
@@ -19,9 +19,6 @@ app = FastAPI()
 
 # 指定したいテーブル名
 table_name = setting.DB_TABLE_NAME
-
-# mysqlコンテナ起動待機
-time.sleep(5)
 
 
 
@@ -40,6 +37,8 @@ async def get_counts_good_cmnt():
         sys.exit(1)
     
     try:
+        # 使用するテーブルを選択
+        cursor.execute(f"USE {table_name}")
         # DBからデータ取得
         cursor.execute("""
             SELECT AVG(likes), AVG(comments)
@@ -79,6 +78,8 @@ async def get_ave_good_topN(n: int = Query(default=10, alias='top_n')):
         sys.exit(1)
     
     try:
+        # 使用するテーブルを選択
+        cursor.execute(f"USE {table_name}")
         # DBからデータ取得
         cursor.execute("""
             SELECT AVG(likes), AVG(comments)
@@ -118,6 +119,8 @@ async def get_ave_flwr_topN(n: int = Query(default=10, alias='top_n')):
         sys.exit(1)
     
     try:
+        # 使用するテーブルを選択
+        cursor.execute(f"USE {table_name}")
         # DBからデータ取得
         cursor.execute("""
             SELECT AVG(likes), AVG(comments)
@@ -157,6 +160,8 @@ async def get_ave_cmnt_topN(n: int = Query(default=10, alias='top_n')):
         sys.exit(1)
     
     try:
+        # 使用するテーブルを選択
+        cursor.execute(f"USE {table_name}")
         # DBからデータ取得
         cursor.execute("""
             SELECT AVG(likes), AVG(comments)
@@ -196,6 +201,8 @@ async def get_noun_topN(n: int = Query(default=10, alias='top_n')):
         sys.exit(1)
     
     try:
+        # 使用するテーブルを選択
+        cursor.execute(f"USE {table_name}")
         # DBからデータ取得
         cursor.execute("""
             SELECT AVG(likes), AVG(comments)
@@ -224,44 +231,44 @@ async def get_noun_topN(n: int = Query(default=10, alias='top_n')):
 #----------------------------------#
 # (テスト)メイン
 #----------------------------------#
-@app.get("/")
-def main():
-    """
-    メイン関数
-    """
+# @app.get("/")
+# def main():
+#     """
+#     メイン関数
+#     """
 
-    # DB接続
-    try:
-        conn, cursor = cmn_session_db.conn_db()
+#     # DB接続
+#     try:
+#         conn, cursor = cmn_session_db.conn_db()
         
-    except Exception:
-        print(f"{cmn_msg.ERR_MSG}DB接続に失敗したためアプリを終了します。")
-        sys.exit(1)
+#     except Exception:
+#         print(f"{cmn_msg.ERR_MSG}DB接続に失敗したためアプリを終了します。")
+#         sys.exit(1)
     
-    # 使用するテーブルを選択
-    cursor.execute(f"USE {table_name}")
+#     # 使用するテーブルを選択
+#     cursor.execute(f"USE {table_name}")
 
-    # DBからデータ取得
-    cursor.execute("""
-        SELECT AVG(likes), AVG(comments)
-        FROM influencers_posts
-    """)
+#     # DBからデータ取得
+#     cursor.execute("""
+#         SELECT AVG(likes), AVG(comments)
+#         FROM influencers_posts
+#     """)
 
-    # クエリ実行結果の取得
-    result = cursor.fetchone()
-    print(result)
-    # クエリ実行結果JSON化
-    # result_json = json.dumps(result, indent=4)
-    # print(result_json)
+#     # クエリ実行結果の取得
+#     result = cursor.fetchone()
+#     print(result)
+#     # クエリ実行結果JSON化
+#     # result_json = json.dumps(result, indent=4)
+#     # print(result_json)
 
-    # DB切断
-    try:
-        cmn_session_db.disconn_db(conn, cursor)
+#     # DB切断
+#     try:
+#         cmn_session_db.disconn_db(conn, cursor)
         
-    except Exception:
-        print(f"{cmn_msg.ERR_MSG}DB切断に失敗したためアプリを終了します。")
-        sys.exit(1)
+#     except Exception:
+#         print(f"{cmn_msg.ERR_MSG}DB切断に失敗したためアプリを終了します。")
+#         sys.exit(1)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()

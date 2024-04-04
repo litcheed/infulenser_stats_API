6b6a406ec7f6740918c6e838a88d5bc0033d455a
litcheed <litcheed@gmail.com>
2024-04-04 22:33:01 +0900

    fix: httpx最新版準拠実装

--- app/test_main_api.py
+++ app/test_main_api.py
@@ -3,13 +3,13 @@ import sys
 sys.path.append("/usr/src/python/app")
 import pytest
 
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 
 from main_api import app
 
 @pytest.mark.anyio
 async def test_get_counts_good_cmnt():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/stats/average-likes-comments")
     assert response.status_code == 200
     assert "average_likes" in response.json()
@@ -18,23 +18,15 @@ async def test_get_counts_good_cmnt():
 
 @pytest.mark.anyio
 async def test_get_ave_good_topN():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/influencers/top-by-likes?top_n=5")
     assert response.status_code == 200
     assert len(response.json()) <= 5
     print("test_get_ave_good_topN passed")
 
 @pytest.mark.anyio
-async def test_get_ave_flwr_topN():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/influencers/top-by-followers?top_n=5")
-    assert response.status_code == 200
-    assert len(response.json()) <= 5
-    print("test_get_ave_flwr_topN passed")
-
-@pytest.mark.anyio
 async def test_get_ave_cmnt_topN():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/influencers/top-by-comments?top_n=5")
     assert response.status_code == 200
     assert len(response.json()) <= 5
@@ -42,7 +34,7 @@ async def test_get_ave_cmnt_topN():
 
 @pytest.mark.anyio
 async def test_get_noun_topN():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/content/top-nouns?top_n=5")
     assert response.status_code == 200
     assert len(response.json()) <= 5
